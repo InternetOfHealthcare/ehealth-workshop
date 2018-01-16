96b708c9d33be1c30c33084a24a5431704ae15c2
Julio Faerman <jfaerman@gmail.com>
2018-01-16 15:31:05 +0100

    Improved anaconda boot

--- ehw_notebook_userdata.sh
+++ ehw_notebook_userdata.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-yum -y install python36
-wget https://s3.amazonaws.com/ehw-pub/Anaconda3-5.0.1-Linux-x86_64.sh -O ./anaconda.sh
+wget https://s3.amazonaws.com/ehw-pub/Anaconda3-5.0.1-Linux-x86_64.sh -O ./anaconda.sh --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 5
 bash ./anaconda.sh -b -p /usr/local/anaconda
 export PATH="/usr/local/anaconda/bin:$PATH"
 source /usr/local/anaconda/bin/activate
@@ -10,4 +9,3 @@ cp jupyter_notebook_config.py /home/ec2-user/.jupyter/jupyter_notebook_config.py
 chown -R ec2-user /home/ec2-user/.jupyter
 sudo -H -u ec2-user bash -c '/usr/local/anaconda/bin/jupyter notebook &'
 echo "notebook userdata finished" 
-
