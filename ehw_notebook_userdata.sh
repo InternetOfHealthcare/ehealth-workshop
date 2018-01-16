c502fe06aa8bcd59e0b92dc9f8ead632e5d7a5d0
Julio Faerman <jfaerman@gmail.com>
2018-01-16 14:11:03 +0100

    Anaconda should work better

--- ehw_notebook_userdata.sh
+++ ehw_notebook_userdata.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 yum -y install python36
-pip-3.6 install jupyter
-jupyter notebook --ip=0.0.0.0 --port=80
+wget https://s3.amazonaws.com/ehw-pub/Anaconda3-5.0.1-Linux-x86_64.sh -O ./anaconda.sh
+bash ./anaconda.sh -b -p /usr/local/anaconda
+export PATH="/usr/local/anaconda/bin:$PATH"
+source /usr/local/anaconda/bin/activate
+jupyter notebook --ip=0.0.0.0 --port80 --no-browser
+
