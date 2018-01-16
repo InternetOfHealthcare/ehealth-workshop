372a8091483f33d9c10dbfad57023e989bc4db9f
Julio Faerman <jfaerman@gmail.com>
2018-01-16 11:56:00 +0100

    Needs git for gitting

--- ehw_notebook_userdata.sh
+++ ehw_notebook_userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum -y install httpd
+yum -y install git httpd
 echo "Hello from $(curl -s http://169.254.169.254/latest/metadata/instance-id)" >> /var/www/html/index.html
 service httpd start
 chkconfig httpd on
